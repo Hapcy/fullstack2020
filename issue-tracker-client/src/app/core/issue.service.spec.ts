db0fec081a8e3abf7bcfd678e12db6ed0ea38c04
tibor.moger <tibor.moger@smartx.eu>
2020-11-30 10:04:02 +0100

    service test

--- issue-tracker-client/src/app/core/issue.service.spec.ts
+++ issue-tracker-client/src/app/core/issue.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { IssueService } from './issue.service';
 
 describe('IssueService', () => {
   let service: IssueService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+    });
     service = TestBed.inject(IssueService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getIssues', () => {
+    it('should call backend for issues', async () => {
+      const issuesPromise = service.getIssues();
+
+      httpMock.expectOne('/api/issues').flush([]);
+
+      expect(await issuesPromise).toEqual([]);
+
+      httpMock.verify();
+    });
   });
 });
